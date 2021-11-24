43abb448f7490774f5c9a05480333ef93bb691fd
maxa93 <nnmaxa@gmail.com>
2021-11-24 16:54:46 +0100

    countCharacters

---
+++ src/CountCharacters.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CountCharacters {
+    public static void main(String []args){
+        String primer="Ovo je prvi primer";
+        int brojkaraktera=primer.length();
+        System.out.println("Broj karaktera "+brojkaraktera);
+        int brojKarakteraBezRazmaka=primer.replace(" ","").length();
+        System.out.println("Broj karaktera bez razmaka "+ brojKarakteraBezRazmaka);
+
+    }
+}
