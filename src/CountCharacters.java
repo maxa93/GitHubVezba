7b99affdf3cacc77e723074ff13aa45f15e440f6
maxa93 <nnmaxa@gmail.com>
2022-01-19 19:16:51 +0100

    Update CountCharacters.java

--- src/CountCharacters.java
+++ src/CountCharacters.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class CountCharacters {
-    public static void main(String []args){
-        String primer="Ovo je prvi primer";
-        int brojkaraktera=primer.length();
-        System.out.println("Broj karaktera "+brojkaraktera);
-        int brojKarakteraBezRazmaka=primer.replace(" ","").length();
-        System.out.println("Broj karaktera bez razmaka "+ brojKarakteraBezRazmaka);
+    public static void main(String[] args) {
+        String primer = "Ovo je prvi primer";
+        int brojkaraktera = primer.length();
+        System.out.println("Broj karaktera " + brojkaraktera);
+        int brojKarakteraBezRazmaka = primer.replace(" ", "").length();
+        System.out.println("Broj karaktera bez razmaka " + brojKarakteraBezRazmaka);
 
     }
 }
+
+
+
+
+
