d2d8047292edfec13c59a684f5bbdb94f40a8f33
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:56:53 +0200

    Create BreakStatement.java

---
+++ src/BreakStatement.java
@@ -0,0 +1,13 @@
+public class BreakStatement {
+    public static void main(String []args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int x : numbers){
+            if (x == 30){
+                break;
+            }
+            System.out.println(x);
+            System.out.println("\n");
+        }
+    }
+}
