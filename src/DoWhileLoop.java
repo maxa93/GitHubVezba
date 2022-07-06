1f3c43079a61192538c2fa92ed59841ae51fb252
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:05 +0200

    Create DoWhileLoop.java

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[]args){
+        int x = 10;
+
+        do {
+            System.out.println("Value of x :" + x);
+            x++;
+            System.out.println("\n");
+        }
+        while (x<20);
+    }
+}
