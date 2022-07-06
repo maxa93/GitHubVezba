6f573399399f41e869455bfe59808bd09da9f8d6
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:07 +0200

    Create EnhancedForLoop.java

---
+++ src/EnhancedForLoop.java
@@ -0,0 +1,16 @@
+public class EnhancedForLoop {
+    public static void main(String []args){
+        int [] numbers = {10,20,30,40,50};
+
+        for (int x : numbers){
+            System.out.println(x);
+            System.out.println(",");
+        }
+        System.out.println("\n");
+        String [] names = {"James","Larry","Tom","Lacy"};
+        for (String name : names){
+            System.out.println(name);
+            System.out.println(",");
+        }
+    }
+}
