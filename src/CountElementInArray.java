9989503b8c15a89c04226791b5fca8bc3dcaca07
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:01 +0200

    Create CountElementInArray.java

---
+++ src/CountElementInArray.java
@@ -0,0 +1,10 @@
+public class CountElementInArray {
+    public static void main(String[]args){
+        String [] mojaLista = {"jedan","dva","tri"};
+        int count = mojaLista.length;;
+        System.out.println("The number of elements are: " + count);
+        for (int i=0; i<mojaLista.length; i++){
+            System.out.println(mojaLista[i]+"");
+        }
+    }
+}
