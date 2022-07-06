271086225ec2cc03d82b2cdc0a3b78fe634df99b
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:11 +0200

    Create IfElseStatement.java

---
+++ src/IfElseStatement.java
@@ -0,0 +1,10 @@
+public class IfElseStatement {
+    public static void main(String[]args){
+        int x = 30;
+        if( x < 20 ) {
+            System.out.print("This is if statement");
+        }else {
+            System.out.print("This is else statement");
+        }
+    }
+}
