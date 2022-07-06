27901f61a26e00975517f960f6409fcdd00a7fc5
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:56:51 +0200

    Create ArraySorting.java

---
+++ src/ArraySorting.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[]args){
+        int[] array = {1, 2, 3, -1, -2, 4};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
