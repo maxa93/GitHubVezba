782e9d87ca578bf6f4df3eab37268291a4e44072
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:56:57 +0200

    Create Count.java

---
+++ src/Count.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Count {
+    public static void main(String[]args){
+        String [] mojaLista = {"jedan","dva","tri"};
+        for (int i=0; i<mojaLista.length; i++){
+            System.out.println(mojaLista[i]+"");
+        }
+    }
+}
