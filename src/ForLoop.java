b527caacc6010f218b3a56e6a23eee3f4669d23e
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:09 +0200

    Create ForLoop.java

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String []args){
+        for(int x = 10; x<20; x++){
+            System.out.println("Value of x " + x);
+            System.out.println("\n");
+        }
+    }
+}
