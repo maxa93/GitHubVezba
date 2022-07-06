82a11caed1d2a61301b4addd914a038d172a2a21
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:13 +0200

    Create IfStatement.java

---
+++ src/IfStatement.java
@@ -0,0 +1,8 @@
+public class IfStatement {
+    public static void main(String[]args){
+        int x = 10;
+        if (x<20){
+            System.out.println("This is if statement");
+        }
+    }
+}
