21110ae0714a21552c70b9a2fa3a9b31b05d051b
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:23 +0200

    Create ReverseString3.java

---
+++ src/ReverseString3.java
@@ -0,0 +1,7 @@
+public class ReverseString3 {
+    public static void main(String[]args) {
+        String s = "sample";
+        String result = new StringBuffer(s).reverse().toString();
+        System.out.println(result);
+    }
+}
