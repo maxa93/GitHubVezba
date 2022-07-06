e5519cf3627314675439bf60346e3e2e717f1815
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:32 +0200

    Create WhileLoop.java

---
+++ src/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[]args){
+        int x = 10;
+
+        while (x < 20){
+            System.out.println("Value of x :" + x);
+            x++;
+            System.out.println("\n");
+        }
+    }
+}
