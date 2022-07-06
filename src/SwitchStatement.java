fe0e311f4662c870846c75af78398ac10cb5fc2c
maxa93 <nnmaxa@gmail.com>
2022-07-06 15:57:30 +0200

    Create SwitchStatement.java

---
+++ src/SwitchStatement.java
@@ -0,0 +1,23 @@
+public class SwitchStatement {
+    public static void main(String[]args){
+        char grade = 'C';
+        switch (grade){
+            case 'A':
+                System.out.println("Exellent");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Well done");
+                break;
+            case 'D':
+                System.out.println("You passed");
+                break;
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
